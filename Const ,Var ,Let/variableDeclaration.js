6cff34a317a39ce6253e1242a4ada1ba2c4a1bc6
Khan Maheebullah Nisar Ahmed <107109005+Maheebullahkhan14@users.noreply.github.com>
2022-11-25 10:19:23 -0800

    Add files via upload

---
+++ Const ,Var ,Let/variableDeclaration.js
@@ -0,0 +1,57 @@
+
+// CONST
+// const person = 'Nick'
+// person = 'john'
+// console.log(person)
+
+//LET
+// let person1 = 'Nick'
+// person1 = 'john'
+// console.log(person1)
+
+
+//VAR
+//Example:1
+// function myfn(){
+//     var myname = 'Gulam'
+// }
+// console.log(myname)
+
+// function myfn2(){
+//     var name = 'John'
+//     if(true){
+//         var name = 'Nick'
+//         console.log(name)
+//     }
+//     console.log(name)
+// }
+// // console.log(name)
+// myfn2()
+
+// function myfunction(){
+//     let myname = 'khan'
+//     if(true){
+//         let myname = 'Gulam';
+//         console.log(myname);
+//     }
+//     console.log(myname)
+// }
+// myfunction()
+// console.log(myname) // Not Defined
+
+// const variables are not immutable ! Concretely, it means that object and array const declared variables can be mutated.
+
+// We Can mutate the Const variables in Objects
+// const person = {
+//     name : 'Khan'
+// }
+// person.name = 'Gulam'
+// console.log(person.name)
+
+// We can mutate the Const varibales in array Also
+
+// const person = []
+
+// person.push('john')
+// console.log(person[0])
+
---
+++ this keyword/this.js
@@ -0,0 +1,21 @@
+// const person = {
+//     firstname : 'Khan',
+//     lastname : 'Maheeb',
+//     id: 5526,
+//     fullname : function(){
+//         return this.firstname + " " + this.lastname
+//         // this Keyword Refers to the object
+//     }
+// }
+
+// console.log(person.fullname())
+
+// function myfn(){
+//     console.log(this)
+//     // this refers to the global object
+// }
+
+// myfn()
+
+let x = this
+console.log(x)
