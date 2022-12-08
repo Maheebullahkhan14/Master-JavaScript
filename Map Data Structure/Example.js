1434beaa5425811441481cbca31e4f6f202c1512
Khan Maheebullah Nisar Ahmed <107109005+Maheebullahkhan14@users.noreply.github.com>
2022-12-08 10:01:49 -0800

    Add files via upload

---
+++ Map Data Structure/Example.js
@@ -0,0 +1,34 @@
+const m = new Map()
+
+// You can add items to the map by using the set method:
+m.set('car' ,'BMW')
+m.set('color' , 'Black')
+
+// And you can get items out of a map by using get:
+const car = m.get('car')
+const color = m.get('color')
+
+// Delete an item from a map by key
+// m.delete('color')
+
+// Delete all items from a map
+// m.clear()
+
+// Check if a map contains an item by key
+// has() method
+
+const hasColor = m.has('color')
+
+// Find the number of items in a map
+const size = m.size
+
+const mynewmapvar = new Map([['color' ,'black'],
+                            ['Car' ,'AstonMartin'],
+                        ['Owner' , 'Khan']])
+
+console.log(mynewmapvar)
+
+
+
+// console.log(size)
+
